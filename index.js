97791899cea7c5a13e3d6ba3bb2ce01d4900e967
Martinpouh <martinchege2015@gmail.com>
2022-06-19 09:21:50 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                name: `${userName}`,
+                email: `${userEmail}`,
+            }),
+        })
+        .then(response => response.json())
+        .then(object => {
+            renderObjId(object)
+        })
+        .catch(error => {
+            renderError(error)
+        })
+}
+
+// handles the POST request response, retrieves the new id value and appends it to the DOM
+function renderObjId(obj) {
+    const newElm = document.createElement('p');
+    newElm.innerHTML = obj.id;
+    document.querySelector('body').appendChild(newElm);
+}
+
+// handles a failed POST request using catch, appends the error message to the DOM
+function renderError(errObj) {
+    const newElm = document.createElement('p');
+    newElm.innerHTML = errObj.message;
+    document.querySelector('body').appendChild(newElm);
+}
